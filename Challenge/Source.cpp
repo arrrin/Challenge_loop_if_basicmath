8582eb09954b804fecca805c1c4d24603a553ff4
ARIN SRITHAWIN <63011063@KMITL.AC.TH>
2020-08-12 21:36:54 +0700

    full code

--- Challenge/Source.cpp
+++ Challenge/Source.cpp
@@ -1,18 +1,41 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 	int a, b;
+	double x, j, sum = 0, sd, sd2;
 	scanf_s("%d %d", &a, &b);
+	float average = (a + b) / 2.0;
 	if (a < b)
 	{
-		for (int i=a; i <= b; i++)
-			printf("%d ", i);
-		int 
+		int count = (b - a);
+		for (int i = a; i <= b; i++) {
+			printf("%d \n", i);
+			x = i - average;
+			j = pow(x, 2);
+			sum += j;
+		}
+		sd = sum / count;
+		sd2 = sqrt(sd);
+		printf("\n");
 	}
+
 	else if (b < a)
 	{
-		for (int i=a;i>=b;i--)
+		int count = (a - b);
+		for (int i = a; i >= b; i--) {
 			printf("%d ", i);
+			x = i - average;
+			j = pow(x, 2);
+			sum += j;
+		}
+		sd = sum / count;
+		sd2 = sqrt(sd);
+		printf("\n");
+
 	}
+
+	printf("Average = %.2f \n", average);
+	printf("sd = % .2lf \n", sd2);
 	return 0;
 }
